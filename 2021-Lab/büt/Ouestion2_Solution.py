85229d7b848b0bdb43149b9ec5be37bab16c9c89
murat1399 <murat1399thersoz@gmail.com>
2024-12-04 14:18:01 +0300

    Add files via upload

---
+++ 2021-Lab/büt/Ouestion2_Solution.py
@@ -0,0 +1,21 @@
+def myfunc():
+    word1 = input("Birinci kelimeyi girin: ").lower()
+    word2 = input("ikinci kelimeyi girin: ").lower()
+    set1 = set()
+    set2 = set()
+    length = len(word1)
+    for char1 in word1 :
+        set1.add(char1)
+    for char2 in word2 :
+        set2.add(char2)
+    if set1 == set2 :
+        num = len(word1)
+        for i in range (length):
+            if word1[i] == word2[i]:
+                num -= 1
+        print("True",num)
+    else :
+        print("False")
+
+myfunc()
+
